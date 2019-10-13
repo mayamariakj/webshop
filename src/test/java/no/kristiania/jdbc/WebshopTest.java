cf648be03cf6224dff3cad25ac5b20ee40c84f69
mayamariakjaer <maya_t_kjaer@hotmail.com>
2019-10-13 15:11:59 +0200

    First commit

---
+++ src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -0,0 +1,32 @@
+package no.kristiania.jdbc;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Random;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class WebshopTest {
+
+    @Test
+    void shouldRetrieveStoredProduct() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test");
+
+        Connection connection = dataSource.getConnection();
+        connection.createStatement().executeUpdate(
+                "create table products(name varchar(100))"
+        );
+
+        ProductDao dao = new ProductDao(dataSource);
+        String productName = pickOne(new String[]{"Apples", "Bananas", "Coconuts", "Dates"});
+        dao.insertProduct(productName);
+        assertThat(dao.listAll()).contains(productName);
+    }
+
+    private String pickOne(String[] strings){ return strings[new Random().nextInt(strings.length)]; }
+
+}
